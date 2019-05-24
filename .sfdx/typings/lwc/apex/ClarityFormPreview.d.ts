92e00c55b2d847a29d5beb442f01cff2dc447da7
Benja Efigenio <befigenio@gmail.com>
2019-05-24 21:25:26 +0300

    rename lookup comps and fremove sample controller

--- .sfdx/typings/lwc/apex/ClarityFormPreview.d.ts
+++ .sfdx/typings/lwc/apex/ClarityFormPreview.d.ts
@@ -5,5 +5,5 @@ declare module "@salesforce/apex/ClarityFormPreview.publishFlow" {
   export default function publishFlow(param: {questionFlow: any}): Promise<any>;
 }
 declare module "@salesforce/apex/ClarityFormPreview.search" {
-  export default function search(param: {searchTerm: any, selectedIds: any}): Promise<any>;
+  export default function search(param: {searchTerm: any, lookupObject: any, selectedIds: any}): Promise<any>;
 }
--- force-app/main/default/lwc/lookup/lookup.js
+++ force-app/main/default/lwc/lookup/lookup.js
@@ -1,3 +1,70 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, api } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
-export default class Lookup extends LightningElement {}
+import lookupSearch from '@salesforce/apex/ClarityFormPreview.search';
+
+export default class Lookup extends LightningElement {
+    
+    // Use alerts instead of toast to notify user
+    @api notifyViaAlerts = false;
+    
+    @track isMultiEntry = false;
+
+    @track initialSelection = [];
+
+    @track errors = [];
+
+    handleLookupTypeChange(event) {
+        this.initialSelection = [];
+        this.errors = [];
+        this.isMultiEntry = event.target.checked;
+    }
+
+    handleSearch(event) {
+        lookupSearch(event.detail)
+            .then(results => {
+                this.template.querySelector('c-lightning-lookup').setSearchResults(results);
+            })
+            .catch(error => {
+                this.notifyUser('Lookup Error', 'An error occured while searching with the lookup field.', 'error');
+                // eslint-disable-next-line no-console
+                console.error('Lookup error', JSON.stringify(error));
+                this.errors = [error];
+            });
+    }
+
+    handleSelectionChange() {
+        this.errors = [];
+    }
+
+    handleSubmit() {
+        this.checkForErrors();
+        if (this.errors.length === 0) {
+            this.notifyUser('Success', 'The form was submitted.', 'success');
+        }
+    }
+
+    checkForErrors() {
+        const selection = this.template.querySelector('c-lookup').getSelection();
+        if (selection.length === 0) {
+            this.errors = [
+                { message: 'You must make a selection before submitting!' },
+                { message: 'Please make a selection and try again.' }
+            ];
+        } else {
+            this.errors = [];
+        }
+    }
+
+    notifyUser(title, message, variant) {
+        if (this.notifyViaAlerts){
+            // Notify via alert
+            // eslint-disable-next-line no-alert
+            alert(`${title}\n${message}`);
+        } else {
+            // Notify via toast
+            const toastEvent = new ShowToastEvent({ title, message, variant });
+            this.dispatchEvent(toastEvent);
+        }
+    }
+}
