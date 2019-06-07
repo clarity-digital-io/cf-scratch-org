7d0c20a0eba946a0d66e532491033c1c788f3f0e
Benja Efigenio <befigenio@gmail.com>
2019-06-07 13:06:02 +0300

    start adding multiple criteria

--- force-app/main/default/lwc/preview/preview.js
+++ force-app/main/default/lwc/preview/preview.js
@@ -10,6 +10,7 @@ export default class Preview extends LightningElement {
     @track form;
     @track formresponseid; 
     @track questions;
+    @track criteria; 
     @track error;
     @track answers = [];
     @track rendered = false;
@@ -18,12 +19,14 @@ export default class Preview extends LightningElement {
 
         preview({ recordId: this.recordId })
             .then(result => {
+
                 this.formresponseid = result['FormResponse'][0].Id;
                 this.form = result['Form'][0];
                 this.styling = this.form.Clarity_Form_Style__r;
-                this.questions = sortQuestions(result['Questions']);
-                this.criteria = this.questions.Clarity_Form_Question_Criteria__r;
-                console.log(this.criteria); 
+                this.questions = sortQuestions(result['Questions']); 
+                this.criteriaController = getCriteriaController(this.questions); //criteria by controlling question
+                this.criteriaControlled = getCriteriaControlled(this.questions); //criteria by controlled question
+
             })
             .catch(error => {
                 this.error = error; 
@@ -46,9 +49,38 @@ export default class Preview extends LightningElement {
 
     formsave({ detail }) {
 
+        this.conditionCheck(detail);
+
         let clean = this.cleanAnswers({ Id: null, Answer__c: detail.Answer__c, Clarity_Form_Question__c: detail.Clarity_Form_Question__c, Clarity_Form_Response__c: this.formresponseid });
 
         detail.save ? this.save(clean) : null;
+
+    }
+
+    conditionCheck(detail) {
+
+        let criteria = this.criteriaController.get(detail.Clarity_Form_Question__c);
+
+        if(criteria != null && criteria.length > 0) {
+
+            this.questions = this.questions.map(question => { 
+
+                if(this.criteriaControlled.get(question.Id) != null) {
+
+                    let conditions = this.criteriaControlled.get(question.Id);
+                    console.log(conditions[0].Value, detail.Answer__c, question);
+                    return { 
+                        ...question, 
+                        Show: conditions[0].Value == detail.Answer__c ? true : false
+                    }
+
+                } else {
+                    return question; 
+                }
+
+            });
+            
+        }
 
     }
 
@@ -61,7 +93,6 @@ export default class Preview extends LightningElement {
     }
 
     save(clean) {
-        console.log('clean', clean);
 
         let match = this.answers.find(answer => answer.Clarity_Form_Question__c == clean.Clarity_Form_Question__c); 
 
@@ -69,7 +100,6 @@ export default class Preview extends LightningElement {
 
         saveAnswer({ answer: JSON.stringify(clean) })
             .then(result => {
-                console.log(result);
                 match ? 
                     this.answers = this.answers.map(answer => {
 
@@ -101,6 +131,49 @@ export default class Preview extends LightningElement {
 
 }
 
+const getCriteriaControlled = (questions) => {
+
+    return questions.reduce((accum, question) => {
+
+        if(question.Clarity_Form_Question_Criteria__r != null && question.Clarity_Form_Question_Criteria__r.length > 0) {
+
+            accum.set(question.Id, question.Clarity_Form_Question_Criteria__r.map(c => {
+                return { QuestionId: c.Clarity_Form_Question__c, Operator: c.Operator__c, Type: c.Type__c, Value: c.Value__c }
+            }));
+
+        }
+
+        return accum; 
+
+    }, new Map());
+
+}
+
+const getCriteriaController = (questions) => {
+
+    return questions.reduce((accum, question) => {
+
+        if(question.Clarity_Form_Question_Criteria__r != null && question.Clarity_Form_Question_Criteria__r.length > 0) {
+
+            question.Clarity_Form_Question_Criteria__r.forEach((criteria, i) => {
+
+                if(accum.get(criteria.Field__c) != null) {
+                    let conditions = accum.get(criteria.Field__c); 
+                    conditions.push({ QuestionId: criteria.Clarity_Form_Question__c, Operator: criteria.Operator__c, Type: criteria.Type__c, Value: criteria.Value__c })
+                    accum.set(criteria.Field__c, conditions);
+                } else {
+                    accum.set(criteria.Field__c, [{ QuestionId: criteria.Clarity_Form_Question__c, Operator: criteria.Operator__c, Type: criteria.Type__c, Value: criteria.Value__c }])
+                }
+
+            })
+
+        }
+
+        return accum;
+
+    }, new Map());
+}
+
 const sortQuestions = (result) => {
 
     let questions = result.sort((a, b) => {
@@ -119,9 +192,10 @@ const sortQuestions = (result) => {
         let nQuestion = { 
             ...question, 
             Clarity_Form_Question_Flow_Designs__r: question.Clarity_Form_Question_Flow_Designs__r[0], 
-            Clarity_Form_Question_Options__r: options != null && options.length ? transformOptions(options) : [] 
+            Clarity_Form_Question_Options__r: options != null && options.length ? transformOptions(options) : [], 
+            Show: question.Clarity_Form_Question_Criteria__r != null && question.Clarity_Form_Question_Criteria__r.length > 0 ? false : true
         };
-        console.log(nQuestion);
+
         return nQuestion;
 
     });
